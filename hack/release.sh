6ee71211e6c8d0f20606a58e18abf5cbf799f2a5
tamal <tamal@appscode.com>
2017-10-11 02:14:05 -0700

    Update release script

--- hack/release.sh
+++ hack/release.sh
@@ -3,6 +3,12 @@
 set -x
 set -eou pipefail
 
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/appscode/kloader"
+rm -rf $REPO_ROOT/dist
+
 env APPSCODE_ENV=prod ./hack/make.py build
 ./hack/make.py push
 env APPSCODE_ENV=prod ./hack/make.py push
+
+rm -rf $REPO_ROOT/dist/.tag
